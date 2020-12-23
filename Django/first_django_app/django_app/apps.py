1bbe03673deb50239f6569ebda6deee9d1367d79
jcchouinard <jcchouinard.mkt@gmail.com>
2020-12-23 14:23:47 +1100

    remove dir

--- Django/first_django_app/django_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoAppConfig(AppConfig):
-    name = 'django_app'
--- Django/first_django_app/django_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'django_app/index.html')
